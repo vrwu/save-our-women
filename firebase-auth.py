2338a6adc3776dd4c2268a234beaa11330511357
vrwu <wu_ruoying@yahoo.com>
2020-07-27 22:55:39 -0700

    firebase auth + html(for test purposes) update

--- firebase-auth.py
+++ firebase-auth.py
@@ -17,30 +17,57 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return render_template("home.html")
 
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
 
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template('signup.html')
+
+    else:
         # retrieves email and pass from user
         email = request.form['name']
         password = request.form['pass']
 
         try: 
              # creates an account on database and requests for verification
-            user=auth.create_user_with_email_and_password(email, password)
+            user = auth.create_user_with_email_and_password(email, password)
             auth.send_email_verification(user['idToken'])
-            return 'Account Created'
-            
-        except:
-            return 'Weak password: Password should be at least 6 characters'
-    return render_template('sign-login.html')
-'''
-def login():
-    email = request.form['name']
-    password = request.form['name']
 
-    user=auth.sign_in_with_email_and_password(email, password)
-'''
+            # returns message for now that leads to deadend but maybe a pop up message instead
+            # needs coordinating with front end
+           # return 'Account Created! Please verify email before signing in'
+            return redirect(url_for('login'))
+
+        except:
+
+            #same as the account created, probably a pop up message
+            return 'Account creation failed. Please ensure a new email or a password of at least 6 characters'
+    return render_template('signup.html', error=error)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        email = request.form['name']
+        password = request.form['pass']
+
+        # issue with email verification, don't know how to check for email verified or not
+        if (auth.User.emailVerified == False):
+            return "Email not verified!"
+            return redirect(url_for('login'))
+        try: 
+            login = auth.sign_in_with_email_and_password(email, password)
+            return "Logged In"
+        except:
+            return "Invalid email and/or password"
+    #return render_template('login.html')
+
 if __name__ == '__main__':
     app.run()
