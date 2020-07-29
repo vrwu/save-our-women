caf789c2b0f1fe51bd11e30a49b501a5c464c1c6
vrwu <wu_ruoying@yahoo.com>
2020-07-28 18:54:23 -0700

    firebase database update

--- firebase-database.py
+++ firebase-database.py
@@ -1,6 +1,20 @@
-from firebase import firebase
+import pyrebase
 
-firebase = firebase.FirebaseApplication("https://save-our-women-b9aef.firebaseio.com/", None)
-data = {
-    'Name': 
-}
+firebaseConfig = {
+    'apiKey': "AIzaSyA1UYeJTygTIPNIBTLd_upZ8EsCjL5iUNs",
+    'authDomain': "save-our-women-b9aef.firebaseapp.com",
+    'databaseURL': "https://save-our-women-b9aef.firebaseio.com",
+    'projectId': "save-our-women-b9aef",
+    'storageBucket': "save-our-women-b9aef.appspot.com",
+    'messagingSenderId': "53046274329",
+    'appId': "1:53046274329:web:92782bf1780b708be63d88",
+    'measurementId': "G-62FZQ6M0FF"
+    }
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+db=firebase.database()
+ 
+#push data
+# data={"name":"John", "age":20, "address":["new york", "los angeles"]}
+# db.push(data)
