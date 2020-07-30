e836fa91c13d9523d6fef433bd728809fddba811
Abbie Yip <adyip@ucsd.edu>
2020-07-29 17:01:48 -0700

    added text box for password

---
+++ Welcome/Password.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput
+} from 'react-native';
+
+export default class password extends Component<{}> {
+  render() {
+    return(
+      <View style={styles.container}>
+        <TextInput style={styles.inputBox}
+          placeholder="password"
+          placeholderTextColor="#FFFFFF"
+        />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+
+  inputBox: {
+    width: 300,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 25,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    color: '#FFFFFF'
+  }
+}
+)
