95efff3c45802cb801676353eb3e2398c03a6007
Abbie Yip <adyip@ucsd.edu>
2020-07-30 15:17:25 -0700

    added navigation element

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import Email from './Welcome/Email';
 import Logo from './Welcome/Logo';
-import LoginButton from './Welcome/LoginButton';
+import HSButtons from './Welcome/HSButtons';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function WelcomeScreen({navigation}) {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Logo/>
       <Text style={styles.SOWText}>save our women</Text>
       <Email/>
-      <LoginButton/>
+      <HSButtons/>
     </View>
+  );
+}
+
+function forgotPassScreen({navigation}){
+  return (
+    <View>
+      <StatusBar style="auto"/>
+      <Logo/>
+      </View>
+    );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="welcome"
+        screenOptions={{
+          headerShown:false
+        }}
+      >
+        <Stack.Screen
+          name="home"
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="forgot password"
+          component={forgotPassScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -23,7 +58,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#9e6590',
     alignItems: 'center',
-    //paddingTop:100,
     paddingBottom:100
   },
 
