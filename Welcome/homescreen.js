fb121c888b0e15419197f52e5f66019b9ce53d23
Abbie Yip <adyip@ucsd.edu>
2020-08-02 12:11:26 -0700

    added navigation bar

--- Welcome/homescreen.js
+++ Welcome/homescreen.js
@@ -1,141 +1,31 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StatusBar } from 'expo-status-bar';
-import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView,
-  Keyboard, Alert, TextInput
- } from 'react-native';
+import {home} from '../Welcome/home'
 
-export function homescreen({navigation}) {
-  return (
-    <View>
-      <View style = {styles.container}>
-      </View>
+const Tab = createBottomTabNavigator();
 
-      <Text style = {styles.homeText}>
-        Home
-      </Text>
-
-      <TouchableOpacity
-        style = {styles.sendAlert}
-      >
-        <Text style = {styles.smallText}>
-          Send Alert
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style = {styles.makeReport}
-      >
-        <Text style = {styles.smallText}>
-          Make Report
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style = {styles.map}
-      >
-        <Text style = {styles.mapText}>
-          Map
-        </Text>
-      </TouchableOpacity>
-      <View style = {styles.navBar}>
-        <TouchableOpacity style = {styles.homeButton}>
-          <Text> home
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
+export default function homeScreen({navigation}) {
+  return(
+    <Tab.Navigator
+    >
+      <Tab.Screen
+        name = "home"
+        component = {home}
+        options={{tabBarLabel:'Home'}}
+      />
+      <Tab.Screen
+        name = "SOS"
+        component = {home}
+        options = {{tabBarLabel:'SOS'}}
+      />
+      <Tab.Screen
+        name = "profile"
+        component = {home}
+        options = {{tabBarLabel:'Profile'}}
+      />
+    </Tab.Navigator>
+  );
 }
-  const styles = StyleSheet.create({
-    container: {
-      top: -10,
-      alignSelf:'center',
-      backgroundColor:'rgba(158, 101, 144, 1)',
-      height: 230,
-      width:420,
-      borderRadius: 50
-    },
-
-    homeText: {
-      fontSize: 20,
-      fontWeight: "bold",
-      alignSelf: 'center',
-      bottom: 125,
-      color: 'white'
-
-    },
-
-    sendAlert: {
-      width: 180,
-      height: 180,
-      position:'absolute',
-      marginLeft: 20,
-      backgroundColor: 'rgba(255, 255, 255, 1)',
-      marginVertical: 150,
-      borderRadius: 25
-    },
-
-    smallText:{
-      fontWeight: 'bold',
-      color: 'grey',
-      textAlign: 'right',
-      right: 20,
-      fontSize: 18,
-      top: 150
-    },
-
-    mapText:{
-      fontWeight: 'bold',
-      color: 'grey',
-      textAlign: 'right',
-      right: 20,
-      fontSize: 18,
-      top: 175
-    },
-
-    makeReport: {
-      width: 180,
-      height: 180,
-      position:'absolute',
-      top: 0,
-      left: 230,
-      marginLeft:-15,
-      backgroundColor: 'rgba(255, 255, 255, 1)',
-      marginVertical: 150,
-      borderRadius: 25
-    },
-
-    map: {
-      width: 378,
-      height: 400,
-      position: 'absolute',
-      backgroundColor: 'white',
-      marginVertical: 300,
-      justifyContent:'center',
-      alignSelf:'center',
-      borderRadius: 25,
-      top: 45
-    },
-
-    navBar: {
-      width: 378,
-      height: 70,
-      backgroundColor:'blue',
-      alignSelf: 'center',
-      justifyContent:'flex-start',
-      top: 507,
-      borderRadius: 25
-    },
-
-    homeButton: {
-      height: 70,
-      width: 70,
-      backgroundColor: 'yellow',
-      marginLeft: 50,
-      marginRight: 50,
-    }
-});
