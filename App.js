8a52a2f6887446887ec523d7f99e18a3bba3de3b
Abbie Yip <adyip@ucsd.edu>
2020-08-02 12:57:32 -0700

    more modules

--- App.js
+++ App.js
@@ -1,37 +1,186 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import Login from './Welcome/Login';
-import SignUp from './Welcome/SignUp';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView,
+  Keyboard, Alert, TextInput, Component
+ } from 'react-native';
+
+import homeScreen from './Welcome/homescreen'
+import Email from './Welcome/Email';
 import Logo from './Welcome/Logo';
-import Password from './Welcome/Password';
+import {forgotPassScreen} from './Welcome/forgotPassScreen'
+
+const VALID_EMAIL = "sow@save-our-women.com"
+const VALID_PASSWORD = "tech-tank"
+
+const Stack = createStackNavigator();
+
+function WelcomeScreen({navigation}) {
+  function handleLogin() {
+    if (Email.email == null) {
+      navigation.navigate('home')
+    }
+  }
+  return (
+    <KeyboardAvoidingView style = {styles.login}>
+      <View style={styles.containerOne}>
+        <StatusBar style="auto" />
+        <Logo/>
+        <Text style={styles.SOWText}>save our women</Text>
+        <Email/>
+        <View>
+          <TouchableOpacity
+            onPress={() =>navigation.navigate('forgot password')}
+          >
+            <Text
+              style={styles.forgotPass}>
+                  Forgot password?</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}
+            onPress = {() => { handleLogin()}}
+          >
+            <Text style={styles.loginText}>
+            Login
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style = {styles.noAcc}
+          //  onPress={() => navigation.navigate('sign up')}
+          >
+            <Text> Don't have an account?</Text>
+            <Text style = {styles.signupText}> Sign Up. </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Logo/>
-      <Text style={styles.SOWLogo}>save our women</Text>
-      <Password/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="welcome"
+        screenOptions={{
+          headerShown:false
+        }}
+      >
+        <Stack.Screen
+          name="welcome"
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="forgot password"
+          component={forgotPassScreen}
+        />
+        <Stack.Screen
+          name = "home"
+          component = {homeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:'center',
+    position: 'relative',
+    alignItems: 'center',
+    paddingBottom:170
+
+  },
+
+
+  inputBox: {
+    width: 300,
+    height: 50,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 25,
+    paddingHorizontal: 13,
+    fontSize: 16,
+    color: '#FFFFFF',
+    marginVertical: 10,
+    top:100,
+  },
+
+  inputBoxTwo: {
+    width: 300,
+    height: 50,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 25,
+    paddingHorizontal: 13,
+    fontSize: 16,
+    color: '#FFFFFF',
+    top: 100
+  },
+
+  login: {
+    flex: 1,
+    justifyContent:'center',
+  },
+
+  containerOne: {
+    flex: 1,
     backgroundColor: '#9e6590',
     alignItems: 'center',
-    //paddingTop:100,
     paddingBottom:100
   },
 
-  SOWLogo: {
+  SOWText: {
     alignItems: 'center',
     color:'#FFFFFF',
     fontSize: 30,
     position: 'absolute',
-    paddingTop: 260
+    paddingTop: 275
   },
+
+  forgotPass: {
+    color:'#FFFFFF',
+    position: 'absolute',
+    left:175,
+    top: -5
+
+  },
+
+  noAcc: {
+    position: 'relative',
+    top:125,
+    left: 75,
+  },
+
+  signupText: {
+    position: 'relative',
+    color: '#FFFFFF',
+    flex:1,
+    paddingLeft: 50,
+    top: 0
+  },
+
+  button: {
+    width: 300,
+    height: 50,
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    borderRadius:25,
+    marginVertical:25,
+    top: 0
+  },
+
+  loginText: {
+    fontSize: 16,
+    paddingTop: 15,
+    textAlign: 'center',
+    color:'#FFFFFF'
+  },
+
+  login: {
+    flex: 1,
+    justifyContent:'center',
+  }
 });
