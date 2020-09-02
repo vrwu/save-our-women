8620d42c67eb5db18e6b0912cb10629a57f30883
Abbie Yip <adyip@ucsd.edu>
2020-09-02 15:36:56 -0700

    navigation error

--- Welcome/tips.js
+++ Welcome/tips.js
@@ -1,29 +1,68 @@
 import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
 
-import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView,
-  Keyboard, Alert, TextInput, Image, Component, Button
-} from 'react-native';
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-export default class tips extends React.Component {
-  render() {
-    return (
-      <View style = {styles.bg}>
-        <Text style = {styles.header}> Tips </Text>
-      </View>
-    )
-  }
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const tips = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
+  return (
+    <View>
+      <Text style = {styles.header}> Tips </Text>
+      <SafeAreaView style = {{top: 150}}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          horizontal={true}
+        />
+      </SafeAreaView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  bg: {
-    backgroundColor:'white',
-    flex:1
-  },
-
   header: {
+    alignSelf: 'center',
     fontSize: 25,
     fontWeight: 'bold',
-    alignSelf: 'center',
-    top: 75
-  }
-})
+    position: 'absolute',
+    top: 100
+  },
+  item: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    height: 250,
+    width: 200,
+    borderRadius: 25
+  },
+
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default tips;
